12cc3698b1a17dae1a173a508f4d5e139463d0fd
Thyago Fernandes <127799223+fthyfernandes@users.noreply.github.com>
2023-09-19 13:20:12 -0300

    add

---
+++ lista-1-poo/lista-1-poo/src/lista_exercicio/Q2.java
@@ -0,0 +1,17 @@
+package lista_exercicio;
+
+public class Q2 {
+    public static void main(String[] args){
+        int a;
+        int soma=0;
+
+        for(a=1;a<=100;a++){
+            if(a%2==0){
+                soma+=a;
+            }
+
+        }
+        System.out.println(soma);
+    }
+    
+}
